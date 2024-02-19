be084d1af553d1dcd30043cc4afd72c1529ba414
Nathan <nathan.cooke@compass.com>
2024-02-19 13:23:49 -0500

    thumbs again

--- keyboards/bastardkb/charybdis/3x5/keymaps/mute-civilian/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/mute-civilian/keymap.c
@@ -5,14 +5,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q   , KC_W  , KC_E    , KC_R  , KC_T       ,                      KC_Y     , KC_U      , KC_I      , KC_O   , KC_P     ,
     KC_CA  , KC_AS , KC_GD   , KC_SF , MOU_G      ,                      MOU_H    , KC_SJ     , KC_GK     , KC_AL  , KC_CSCLN ,
     NAV_Z  , KC_X  , KC_C    , KC_V  , KC_B       ,                      KC_N     , KC_M      , KC_COMM   , KC_DOT , SYM_SLSH ,
-                     SYM_ENT , KC_NO , WORDS_BACK ,                      MISC_DEL , NAV_SPACE
+                     /* SYM_ENT , KC_NO , WORDS_BACK ,                      MISC_DEL , NAV_SPACE */
+                     WORDS_BACK, KC_NO , SYM_ENT ,                      NAV_SPACE, MISC_DEL
   ),
 
   [_MISC] = LAYOUT_charybdis_3x5(
     _______  , _______ , DESKTOP , _______ , _______ ,                   AM_CYCLE   , _______  , _______ , _______  , QK_BOOT  ,
     AM_LEFT  , DESK_L  , MISSION , DESK_R  , AM_RITE ,                   AM_INC_PAN , AM_1     , AM_2    , AM_3     , AM_CCW   ,
     KC_LCTL  , _______ , _______ , _______ , _______ ,                   AM_DEC_PAN , MS_BACK  , MS_FORE , TAB_BACK , TAB_FORE ,
-                         ZOO_MIC , KC_NO   , ZOO_CAM ,                   _______    , ZOO_HAND
+                         /* ZOO_MIC , KC_NO   , ZOO_CAM ,                   _______    , ZOO_HAND */
+                         ZOO_CAM , KC_NO   , ZOO_MIC ,                   ZOO_HAND    , _______
   ),
 
   [_MOUSE] = LAYOUT_charybdis_3x5(
@@ -26,7 +28,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______ , KC_F6      , ALT_U     , _______ , _______  ,             GUI_L      , KC_HOME , KC_END     , GUI_R , _______ ,
     _______ , WEB_B      , ALT_D     , WEB_F   , _______  ,             LEFT       , KC_DOWN , KC_UP      , RIGHT , _______ ,
     _______ , SL_CL_RPAN , SL_EMOJI  , SL_DMS  , SL_LATER ,             ALT_L      , _______ , _______    , ALT_R , _______ ,
-                           SL_THREAD , KC_NO   , SL_HOME  ,             SCREEN_CAP , _______
+                           /* SL_THREAD , KC_NO   , SL_HOME  ,             SCREEN_CAP , _______ */
+                           SL_HOME , KC_NO   , SL_THREAD  ,             _______ , SCREEN_CAP
   ),
 
   [_SYMBOLS] = LAYOUT_charybdis_3x5(
@@ -40,7 +43,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_SEC_1 , KC_SEC_2 , KC_SEC_5   , KC_SEC_9 , KC_SEC_10 ,            _______ , _______ , _______ , _______ , SARCASM ,
     KC_SEC_3 , KC_SEC_4 , _______    , _______  , _______   ,            _______ , CAPS    , _______ , _______ , _______ ,
     KC_SEC_7 , KC_SEC_6 , KC_SEC_8   , _______  , _______   ,            _______ , _______ , _______ , _______ , _______ ,
-                          LGUI(KC_0) , KC_NO    , _______   ,            _______ , _______
+                          /* LGUI(KC_0) , KC_NO    , _______   ,            _______ , _______ */
+                          _______ , KC_NO , LGUI(KC_0) ,             _______ , _______
   ),
 
   /* [_TEMPLATE] = LAYOUT_charybdis_3x5( */
