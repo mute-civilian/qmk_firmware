b22ab39074e88027832d1b4f1a336943ea88ad85
Nathan <nathan.cooke@compass.com>
2024-12-02 09:26:10 -0500

    config for office keeb

--- keyboards/bastardkb/charybdis/3x5/keymaps/mute-civilian/config.h
+++ keyboards/bastardkb/charybdis/3x5/keymaps/mute-civilian/config.h
@@ -3,24 +3,25 @@
 #define CHARYBDIS_DRAGSCROLL_REVERSE_Y
 #define CHARYBDIS_MINIMUM_SNIPING_DPI 400
 
-#if defined(KEYBOARD_bastardkb_charybdis_3x5_v2_splinky_3)
-  #define CHARYBDIS_MINIMUM_DEFAULT_DPI 1750
-  #undef ROTATIONAL_TRANSFORM_ANGLE
-  #define ROTATIONAL_TRANSFORM_ANGLE -45
-#else
-  #define CHARYBDIS_MINIMUM_DEFAULT_DPI 1200
-#endif
+/* home keeb*/
+/* #define CHARYBDIS_MINIMUM_DEFAULT_DPI 1750 */
+/* office keeb */
+#define CHARYBDIS_MINIMUM_DEFAULT_DPI 2000
+#undef ROTATIONAL_TRANSFORM_ANGLE
+#define ROTATIONAL_TRANSFORM_ANGLE -45
 
 #ifdef ENCODER_ENABLE
+/* home keeb */
+  /* #define ENCODER_RESOLUTIONS { 4 } */
+/* office keeb */
+  #define ENCODER_RESOLUTIONS { 2 }
 
-  #if defined(KEYBOARD_bastardkb_charybdis_3x5_v2_splinky_3)
-    #define ENCODERS_PAD_A { GP23 }
-    #define ENCODERS_PAD_B { GP22 }
-    #define ENCODER_RESOLUTIONS { 2 }
-  #else
+  #if !(defined(KEYBOARD_bastardkb_charybdis_3x5_v2_splinky_3))
     #define ENCODERS_PAD_A { B2 }
     #define ENCODERS_PAD_B { B1 }
-    #define ENCODER_RESOLUTIONS { 4 }
+  #else
+    #define ENCODERS_PAD_A { GP23 }
+    #define ENCODERS_PAD_B { GP22 }
   #endif
 
   #define ENCODERS_PAD_A_RIGHT {  }
